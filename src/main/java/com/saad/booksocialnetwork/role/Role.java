883e2c6353a9b69e00fd08199b9222002fbe2f99
saad.qayyum <khawajasaadqayyum4110@gmail.com>
2024-09-03 09:56:51 +0300

    Role class is added and GrantedAuthority method is also implemented in user

---
+++ src/main/java/com/saad/booksocialnetwork/role/Role.java
@@ -0,0 +1,37 @@
+package com.saad.booksocialnetwork.role;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.saad.booksocialnetwork.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+    @JsonIgnore
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false , updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}

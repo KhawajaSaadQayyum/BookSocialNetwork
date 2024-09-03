40d1ac2eaee03d89631e15d1c46e7f6bd199ae50
saad.qayyum <khawajasaadqayyum4110@gmail.com>
2024-09-03 08:56:14 +0300

    Till Now Infrastructure for the application is up and running. Means Database connectivity and java mail sender are up and running. Docker compose files are added.

---
+++ src/main/java/com/saad/booksocialnetwork/BookSocialNetworkApplication.java
@@ -0,0 +1,14 @@
+package com.saad.booksocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSocialNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSocialNetworkApplication.class, args);
+        System.out.println("Keep Learning and Keep Shining");
+    }
+
+}
